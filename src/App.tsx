a57421111b5bb8d53825e4d7c3c13ce843308b89
boflaherty3 <brendenoflaherty@gmail.com>
2025-03-11 11:36:35 -0400

    going back to hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -10,7 +10,7 @@ import Contact from "./pages/Contact";*/
 
 function App() {
   return (
-    <Router basename="/brenden-portfolio">
+    <Router>
       <div className="min-h-screen flex flex-col bg-gray-100">
         <Navbar />
         <main className="flex-grow">

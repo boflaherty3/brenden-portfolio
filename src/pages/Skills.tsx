ee6776a06fc981c91bc7d152722238bb83429dca
boflaherty3 <brendenoflaherty@gmail.com>
2025-03-13 13:25:43 -0400

    changing layout on skills page

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -18,15 +18,16 @@ const Skills = () => {
         "Google Analytics",
         "WordPress",
         "HTML",
+        "DNS Configuration",
     ];
 
     return(
         <section className="min-h-screen py-12 bg-gray-900 text-white overflow-x-hidden">
             <div className="container mx-auto">
                 <h2 className="text-3xl font-bold text-center mb-8 text-white">Skills</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="flex flex-wrap justify-center gap-6 my-8">
                     {skills.map((skills, index) => (
-                    <div key={index} className="bg-gray-800 text-blue-500 px-4 py-2 rounded-full text-md font-semibold hover:bg-blue-500 hover:text-white transition-colors">{skills}</div>
+                    <div key={index} className="bg-gray-800 text-blue-500 px-4 py-2 rounded-full text-md lg:text-xl font-semibold hover:bg-blue-500 hover:text-white transition-colors">{skills}</div>
                 ))}
                 </div>
             </div>
